89110f691dbf03849257b55984b1a78c0b0a19a1
yober cieza <gctic.sgsass12@essalud.gob.pe>
2024-01-28 18:45:08 -0500

    nueva app

---
+++ app.js
@@ -0,0 +1,98 @@
+/*Este sistema me permite evaluar un numero */
+//Variables
+/*let contador=0;
+let numerMax=prompt("Ingresa un numero maximo a repetir")
+while(contador<=numerMax){
+
+    //prompt("Me indicas un del 1 al 10 por favor:");   
+    alert(`El numero es: ${contador}`);
+    //console.log(contador)
+    contador=contador+1;
+    
+}
+*/
+/*
+let numeroSecreto = Math.floor(Math.random()*10)+1;
+let numeroUsuario = 0;
+let intentos=1;
+//let palabraVeces='vez';
+while(numeroUsuario != numeroSecreto){
+ numeroUsuario = prompt("Me indicas un del 1 al 10 por favor:");
+
+console.log(typeof( numeroUsuario));
+//Evalua si cumple con la condicion
+
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste el numero es: ${numeroUsuario}. lo hiciste en ${intentos} ${ intentos == 1 ? 'ves':'veces'}`);
+        
+    }else{
+        if(numeroUsuario>numeroSecreto){
+         alert('El numero secreto es menor')
+         }else{
+              alert('El numero secreto es mayor')
+        }
+   //intentos =intentos+1;
+   //intentos +=1;
+   intentos++ ;
+   palabraVeces='veces';
+   if(intentos>3){
+    alert("Llegaste al numero maximo de intentos")
+    break;
+   }
+    }
+}
+
+let nombre='Yober'
+console.log(`¡Hola,${nombre}!`);
+alert(`¡Hola,${nombre}!`);
+let lenguajeProgmacion;
+lenguajeProgmacion=prompt('¿Cuál es el lenguaje de programación que más te gusta? ');
+console.log(lenguajeProgmacion);
+
+let valor1=7;
+let valor2=5;
+let resultado;
+resultado=valor1-valor2;
+console.log(`La diferencia  entre ${valor1} y ${valor2} es igual a ${resultado}`)
+
+
+let edad=prompt('Ingrese tu edad');
+if(edad>=18){
+    alert('Ya es mayor de edad');
+}else{
+    alert('Aun es menor de edad')
+}
+
+let numero=prompt('Ingresa un numero');
+if(numero>0){
+    alert('El numero es positivo');
+}else{
+    if(numero==0){
+        alert('el numero es 0');
+    }else{
+        alert('El numero es negativo')
+    }
+}
+
+
+let  numero;
+  numero=1;
+while(numero <= 10){
+    alert(`El numero es ${numero}`);
+    numero++;
+
+}
+
+let nota=7;
+if(nota>=7){
+    alert('Aprobado');
+}else{
+    alert('Desaprobado');
+}
+*/
+let numeroAleatorio=alert(Math.floor( Math.random()*1000)+1);
+
+
+
+
+
